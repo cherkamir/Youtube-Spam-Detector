43cfd0b460492d8dccbdf9ce3d87ec59450f2bf9
Amir Cherkaoui <amircherk@Amirs-MacBook-Pro.local>
2022-05-26 21:35:00 -0400

    adding file

---
+++ youtube-spam-detector.py
@@ -0,0 +1,9 @@
+'''
+1. Retrieve the comments and the user to create a hashMap with key = User (which is probably going to be the url to his youtube channel)
+and value = Number of spam comments.
+2. Sort the hashMap from highest to lowest amount of spams.
+
+
+Shouldn't be that hard :))
+'''
+
